0cd590bfce594a5263fd08214edeeb5ceb573360
Petr Nymsa <nymsa.petr@outlook.cz>
2019-12-01 18:34:42 +0100

    WIP: Refactor assigner - hardly testable
    GH assigner class is heavy and has tight coupling to click and requests packages
    - gh assigner swallowing exceptions and directly writing to console output (via click)
    - gh assigner forcibly quit program (exit)
    - no way to see if desired action is taken without mocking click

--- ghia/assigner.py
+++ ghia/assigner.py
@@ -2,15 +2,14 @@ import requests
 import click
 import json
 import ghia.issue as iss
-from .rule import Rule, RuleSet
 
 
 class GitHubIssueAssigner:
-    def __init__(self, token, repo, strategy):
+    def __init__(self, token, repo, session=None, strategy='append'):
         self.strategy = strategy
         self.token = token
         self.repo = repo
-        self.session = requests.Session()
+        self.session =  session or requests.Session()
         self.session.auth = self._req_token_auth
         self.issues = []
 
@@ -23,6 +22,38 @@ class GitHubIssueAssigner:
 
         for json_issue in loaded_issues:
             self.issues.append(iss.Issue.from_json(json_issue))
+
+    def _patch_issue(self, issue: iss.Issue):
+        try:
+            encoded = json.dumps(issue.patched_dict())
+            r = self.session.patch(
+                f'https://api.github.com/repos/{self.repo}/issues/{issue.number}', data=encoded)
+            r.raise_for_status()
+            return True
+        except:
+            return False
+
+    def _apply_strategy(self, issue: iss.Issue, owners):
+        if self.strategy == 'append':
+            return issue.append(owners)
+        elif self.strategy == 'set':
+            return issue.replace(owners)
+        elif self.strategy == 'change':
+            return issue.clear_add_reapply(owners)
+        else:
+            raise Exception(f'Unknown strategy {self.strategy}')
+
+    def _check_fallback(self, issue: iss.Issue, fallback):
+         # apply fallback if needed
+        if not issue.assignees and fallback is not None:
+            return issue.apply_label(fallback)
+        return []
+
+    def _has_changes(self, changes):
+        return any(x.change_type != iss.CHANGE_REMAIN for x in changes)
+
+    def set_strategy(self, strategy):
+        self.strategy = strategy
 
     def load_issues(self):
         try:
@@ -45,57 +76,27 @@ class GitHubIssueAssigner:
                 f': Could not list issues for repository {self.repo}', err=True)
             exit(10)
 
-    def patch_issue(self, issue: iss.Issue):
-        try:
-            encoded = json.dumps(issue.patched_dict())
-            r = self.session.patch(
-                f'https://api.github.com/repos/{self.repo}/issues/{issue.number}', data=encoded)
-            r.raise_for_status()
-            return True
-        except:
-            return False
-
-    def apply_strategy(self, issue: iss.Issue, owners, fallback):
-        if self.strategy == 'append':
-            return issue.append(owners)
-        elif self.strategy == 'set':
-            return issue.replace(owners)
-        elif self.strategy == 'change':
-            return issue.clear_add_reapply(owners)
-        else:
-            raise Exception(f'Unknown strategy {self.strategy}')
-
-    def check_fallback(self, issue: iss.Issue, fallback):
-         # apply fallback if needed
-        if not issue.assignees and fallback is not None:
-            return issue.apply_label(fallback)
-        return []
-
-    def has_changes(self, changes):
-        return any(x.change_type != iss.CHANGE_REMAIN for x in changes)
-
-    def proces_issues(self, rules, fallback, dry_run: bool):
+    def process(self, rules, fallback, dry_run: bool):
         for issue in self.issues:
-            changes = []
             applies = []
             for rule in rules:
                 if rule.validate(issue):
                     applies.append(rule.owner)
 
-            changes = self.apply_strategy(issue, applies, fallback)
-            c = self.check_fallback(issue, fallback)
+            changes = self._apply_strategy(issue, applies)
+            c = self._check_fallback(issue, fallback)
             for i in c:
                 changes.append(i)
 
             changes.sort(key=lambda x: x.name.lower())
 
-            if not dry_run and self.has_changes(changes) and not self.patch_issue(issue):
+            if not dry_run and self._has_changes(changes) and not self._patch_issue(issue):
                 changes = [iss.IssueChange(
                     iss.CHANGE_ERROR, f'Could not update issue {self.repo}#{issue.number}')]
 
-            self.print_result(issue.number, issue.url, changes)
+            self._print_result(issue.number, issue.url, changes)
 
-    def print_result(self, issue_id: str, url: str, changes=[]):
+    def _print_result(self, issue_id: str, url: str, changes=[]):
         click.echo('-> ', nl=False)
         click.secho(f'{self.repo}#{issue_id}', bold=True, nl=False)
         click.secho(f' ({url})')
--- tests/test_assigner.py
+++ tests/test_assigner.py
@@ -1,10 +1,22 @@
 import betamax
+import os
+import pytest
+from ghia.assigner import GitHubIssueAssigner
+
+api_token = os.environ.get('GITHUB_TOKEN', 'fizztoken')
+api_repo = os.environ.get('GITHUB_REPO', 'repotest')
+
 
 with betamax.Betamax.configure() as config:
-    # tell Betamax where to find the cassettes
-    # make sure to create the directory
     config.cassette_library_dir = 'tests/fixtures/cassettes'
+    # config.default_cassette_options['serialize_with'] = 'prettyjson'
+    config.define_cassette_placeholder('<AUTH_TOKEN>', api_token)
 
+@pytest.fixture
+def client(betamax_session):
+    return GitHubIssueAssigner(api_token, api_repo,betamax_session)
 
-def test_get(betamax_session):
-    betamax_session.get('https://www.seznam.cz')
+@pytest.fixture
+def fake_client(betamax_session):
+    return GitHubIssueAssigner('faketoken', 'fakerepo', betamax_session)
+
